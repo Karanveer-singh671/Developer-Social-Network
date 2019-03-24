3c35827d49aa1d62a54bc75b1c570bef3f3a43f0
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-24 13:15:57 -0700

    login functionality working isAuthenticated and got token (has authorization header) will go away on refresh so need to persist checkt o see if token in ls still TODo

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authActions';
+import classnames from 'classnames';
 
 class Login extends Component {
 	constructor() {
@@ -21,13 +25,28 @@ class Login extends Component {
 	// when submission of form, take the changed value on the inputs and put those in a new user object
 	onSubmit = (e) => {
 		e.preventDefault();
-		const User = {
+		const UserData = {
 			email: this.state.email,
 			password: this.state.password
 		};
+		// call action when u submit
+		this.props.loginUser(userData);
 	};
 
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.auth.isAuthenticated) {
+			this.props.history.push('/dashboard');
+		}
+		if (nextProps.errors) {
+			this.setState({
+				errors: nextProps.errors
+			});
+		}
+	}
+
 	render() {
+		const { errors } = this.state;
+
 		return (
 			<div className="login">
 				<div className="container">
@@ -39,22 +58,28 @@ class Login extends Component {
 								<div className="form-group">
 									<input
 										type="email"
-										className="form-control form-control-lg"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.email
+										})}
 										placeholder="Email Address"
 										name="email"
 										value={this.state.email}
 										onChange={this.onChange}
 									/>
+									{errors.email && <div className="invalid-feedback">{errors.email}</div>}
 								</div>
 								<div className="form-group">
 									<input
 										type="password"
-										className="form-control form-control-lg"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password
+										})}
 										placeholder="Password"
 										name="password"
 										value={this.state.password}
 										onChange={this.onChange}
 									/>
+									{errors.password && <div className="invalid-feedback">{errors.password}</div>}
 								</div>
 								<input type="submit" className="btn btn-info btn-block mt-4" />
 							</form>
@@ -65,5 +90,16 @@ class Login extends Component {
 		);
 	}
 }
+Login.propTypes = {
+	loginUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired
+};
 
-export default Login;
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+	errors: state.auth
+});
+
+// mapStateToProps and function want to call from action file
+export default connect(mapStateToProps, { loginUser })(Login);
