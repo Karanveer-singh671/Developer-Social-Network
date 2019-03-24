c09f4e940abd67ed7c4135eccf5fd471635179b8
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-24 13:21:10 -0700

    setCurrent user for login so now on refresh will persist, on logout need to logout still

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -10,6 +9,19 @@ import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import store from './store';
+import { jwt_decode } from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
+// check for token
+if (localStorage.jwtToken) {
+	// set auth token header auth
+	setAuthToken(localStorage.jwtToken);
+	// decode token and get user info and exp
+	const decoded = jwt_decode(localStorage.jwtToken);
+	// set user and isAuthenticated
+	store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
 	render() {
