b00602f2b872e95d98b0fc9f02972a18ae18381c
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-25 15:45:22 -0700

    profile about component functions and pulls data of user from db now

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -34,7 +34,7 @@ class Profile extends Component {
 						</div>
 						<div className="col-md-6" />
 					</div>
-					{/* pass in profile property to ProfileHeader */}
+					{/* pass in profile property to ProfileHeader, cred etc can access thru this.props now */}
 					<ProfileHeader profile={profile} />
 					<ProfileAbout profile={profile} />
 					<ProfileCreds education={profile.education} experience={profile.experience} />
