5af67d657dc8f78eb67a133b7aafee2e6cfdc240
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-25 15:34:42 -0700

    profile markup done, pass props to subcomponents need to add markup for those, added spinner if null profile or loading

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -18,12 +18,38 @@ class Profile extends Component {
 	}
 
 	render() {
+		const { profile, loading } = this.props.profile;
+		let profileContent;
+
+		if (profile === null || loading) {
+			profileContent = <Spinner />;
+		} else {
+			profileContent = (
+				<div>
+					<div className="row">
+						<div className="col-md-6">
+							<Link to="/profiles" className="btn btn-light mb-3 float-left">
+								Back To Profiles
+							</Link>
+						</div>
+						<div className="col-md-6" />
+					</div>
+					{/* pass in profile property to ProfileHeader */}
+					<ProfileHeader profile={profile} />
+					<ProfileAbout profile={profile} />
+					<ProfileCreds education={profile.education} experience={profile.experience} />
+					{profile.githubusername ? <ProfileGithub username={profile.githubusername} /> : null}
+				</div>
+			);
+		}
+
 		return (
-			<div>
-				<ProfileHeader />
-				<ProfileAbout />
-				<ProfileGithub />
-				<ProfileCreds />
+			<div className="profile">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">{profileContent}</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
