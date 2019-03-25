d974ce30cfe939d448613b111406cd5f06b6184b
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-25 13:36:45 -0700

    edit profile page functionality done

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -6,7 +6,10 @@ import TextAreaFieldGroup from './../common/TextAreaFieldGroup';
 import InputGroup from './../common/InputGroup';
 import SelectListGroup from './../common/SelectListGroup';
 import { withRouter } from 'react-router-dom';
-import { createProfile } from '../../actions/profileActions';
+import { createProfile, getCurrentProfile } from '../../actions/profileActions';
+import CreateProfile from './EditProfile';
+import { getCurrentProfile } from './../../actions/profileActions';
+import isEmpty from './../../validation/is-empty';
 
 class CreateProfile extends Component {
 	constructor(props) {
@@ -56,9 +59,52 @@ class CreateProfile extends Component {
 			[e.target.name]: e.target.value
 		});
 	};
+	//first thing is to get current profile to edit
+	componentDidMount() {
+		this.props.getCurrentProfile();
+	}
+
 	componentWillReceiveProps(nextProps) {
 		if (nextProps.errors) {
 			this.setState({ errors: nextProps.errors });
+		}
+		// check for profile
+		if (nextProps.profile.profile) {
+			const profile = nextProps.profile.profile;
+
+			// CHANGE skills array to CSV
+			// take array turn to string and separate by comma
+			const skillsCSV = profile.skills.join(',');
+
+			// if profile field DNE make into empty string
+			profile.company = !isEmpty(profile.company) ? profile.company : '';
+			profile.website = !isEmpty(profile.website) ? profile.website : '';
+			profile.location = !isEmpty(profile.location) ? profile.location : '';
+			profile.githubusername = !isEmpty(profile.githubusername) ? profile.githubusername : '';
+			profile.bio = !isEmpty(profile.bio) ? profile.bio : '';
+			profile.social = !isEmpty(profile.social) ? profile.social : {};
+			profile.twitter = !isEmpty(profile.social.twitter) ? profile.social.twitter : '';
+			profile.facebook = !isEmpty(profile.social.facebook) ? profile.social.facebook : '';
+			profile.linkedin = !isEmpty(profile.social.linkedin) ? profile.social.linkedin : '';
+			profile.youtube = !isEmpty(profile.social.youtube) ? profile.social.youtube : '';
+			profile.instagram = !isEmpty(profile.social.instagram) ? profile.social.instagram : '';
+
+			// Set component fields state, still in if statement
+			this.setState({
+				handle: profile.handle,
+				company: profile.company,
+				website: profile.website,
+				location: profile.location,
+				status: profile.status,
+				skills: skillsCSV,
+				githubusername: profile.githubusername,
+				bio: profile.bio,
+				twitter: profile.twitter,
+				facebook: profile.facebook,
+				linkedin: profile.linkedin,
+				youtube: profile.youtube,
+				instagram: profile.instagram
+			});
 		}
 	}
 
@@ -133,10 +179,7 @@ class CreateProfile extends Component {
 					<div className="row">
 						{' '}
 						<div className="col-md-8 m-auto">
-							<h1 className="display-4 text-center">Create your Profile</h1>
-							<p className="lead text-center">
-								Let's get some information about you here to make your profile pop!
-							</p>
+							<h1 className="display-4 text-center">Edit your Profile</h1>
 							<small className="d-block pb-3">* = required fields</small>
 							<form onSubmit={this.onSubmit}>
 								<TextFieldGroup
@@ -231,6 +274,8 @@ class CreateProfile extends Component {
 	}
 }
 CreateProfile.propTypes = {
+	CreateProfile: PropTypes.func.isRequired,
+	getCurrentProfile: PropTypes.func.isRequired,
 	profile: PropTypes.object.isRequired,
 	errors: PropTypes.object.isRequired
 };
@@ -240,4 +285,4 @@ const mapStateToProps = (state) => ({
 	errors: state.errors
 });
 
-export default connect(mapStateToProps, { createProfile })(withRouter(CreateProfile));
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(withRouter(CreateProfile));
