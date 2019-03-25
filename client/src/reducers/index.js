5d6104d8f74993265d72371f08ba4e6ca5b4e157
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-25 16:34:18 -0700

    postAdd action done

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
 import profileReducer from './profileReducer';
-
+import postReducer from './postReducer';
+// need to add all reducers in this file or else will not be seen!
 export default combineReducers({
 	auth: authReducer,
 	errors: errorReducer,
-	profile: profileReducer
+	profile: profileReducer,
+	post: postReducer
 });
