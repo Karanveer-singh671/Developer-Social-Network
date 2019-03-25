95dcf82f5126af94e92e97dc106db85cf662cbe8
Karanveer Singh <singhkaranveer671@gmail.com>
2019-03-25 15:24:38 -0700

    getProfileByHandle action created

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ProfileAbout from './ProfileAbout';
+import ProfileHeader from './ProfileHeader';
+import ProfileCreds from './ProfileCreds';
+import ProfileGithub from './ProfileGithub';
+import Spinner from './../common/Spinner';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { getProfileByHandle } from '../../actions/profileActions';
+
+class Profile extends Component {
+	componentDidMount() {
+		// check thru URI endpoint
+		if (this.props.match.params.handle) {
+			this.props.getProfileByHandle(this.props.match.params.handle);
+		}
+	}
+
+	render() {
+		return <div />;
+	}
+}
+
+Profile.propTypes = {
+	getProfileByHandle: PropTypes.func.isRequired,
+	profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+	profile: state.profile
+});
+
+export default connect(mapStateToProps, { getProfileByHandle })(Profile);
